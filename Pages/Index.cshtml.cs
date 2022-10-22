d49be1731daf7f26fbeddb8b1f49c7a3c0c83240
Lisbet Enciso <mlisbet.enciso@gmail.com>
2022-10-21 19:37:42 -0500

    Add files via upload
    Signed-off-by: Lisbet Enciso <mlisbet.enciso@gmail.com>

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Runtime.InteropServices;
+
+public class IndexModel 
+{
+
+    public string OSVersion { get { return RuntimeInformation.OSDescription; }  }
+    
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {        
+    }
+} 
